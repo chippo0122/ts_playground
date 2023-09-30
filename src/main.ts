91b5ed103c916ef713e5defbc5dc6d3db8fa9acf
陳震庭 <chenzhenting@zhenting.local>
2023-09-30 12:22:19 +0800

    interface practice

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,33 @@
 import './style.css';
 
+const arr: Array<string> = ['123', '1332', '123'];
+
+let greet: Function;
+interface Person {
+    name: string,
+    age: number,
+    hobbit?: string
+};
+
+const tom: Person = {
+    name: 'tom',
+    age: 26,
+    hobbit: 'read'
+}
+const tim: Person = {
+    name: 'tim',
+    age: 20
+}
+const aurora: Person = {
+    name: 'aurora',
+    age: 28,
+    hobbit: 'cycling'
+}
+
+greet = (user: Person) => {
+    console.log(`Hi, i'm ${user.name}, i'm ${user.age} years old. ${user.hobbit ? `i hava a hobbit, it is ${user.hobbit}` : ""}`);
+}
+
+greet(tom);
+greet(tim);
+greet(aurora);
